f9bcfd67e530cd92461a2ab8886de9424b0d075a
Andzhey Evula <aevula@mail.ru>
2024-07-20 19:38:31 +0300

    Dedski-14 fixed issues, reusable workflow from master

--- lib/api/airtable.rb
+++ lib/api/airtable.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
 require_relative 'client'
-require 'pp'
 
 class Airtable
   extend Client
 
   def self.send_webhook(url:, data:)
-    pp '*' * 100
-    pp url.inspect
-    pp '*' * 100
-    pp ENV.inspect
-    pp '*' * 100
     client(url:).post(nil, data)
   end
 end
