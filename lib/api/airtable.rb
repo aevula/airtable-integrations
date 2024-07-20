fe739980864bf5b4a3fa193c0ecaa97c942358d1
Andzhey Evula <aevula@mail.ru>
2024-07-20 19:20:29 +0300

    Dedski-14 temporary

--- lib/api/airtable.rb
+++ lib/api/airtable.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 require_relative 'client'
+require 'pp'
 
 class Airtable
   extend Client
 
   def self.send_webhook(url:, data:)
+    pp '*' * 100
+    pp url.inspect
+    pp '*' * 100
+    pp ENV.inspect
+    pp '*' * 100
     client(url:).post(nil, data)
   end
 end
